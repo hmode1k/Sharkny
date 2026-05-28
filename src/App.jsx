2e4128caf48c5f4c9a26507a22b3736031154da5
hmode1k <hmode288277@gmail.com>
2026-05-28 17:59:42 +0300

    ux improvment faster transitions

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,16 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
 import CardContainer from "./components/CardContainer";
-import { useEffect, useState } from "react";
-import { supabase } from "./supabase-client";
+
 import { useNavigate, useParams } from "react-router";
 import AsideWrapper from "./components/AsideWrapper";
 
 function App() {
-  const [userId, setUserId] = useState(null);
-  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
   const { category } = useParams();
   console.log("param", category);
-  useEffect(() => {
-    const fetchUser = async () => {
-      const {
-        data: { user },
-      } = await supabase.auth.getUser();
-      setUserId(user.id);
-      setLoading(false);
-    };
 
-    fetchUser();
-  });
-
-  return loading ? (
-    <>loading</>
-  ) : (
+  return (
     <>
       <NavBar></NavBar>
       <div className="h-full w-full sm:grid sm:grid-cols-[150px_minmax(200px,_1fr)]">
@@ -36,7 +20,6 @@ function App() {
             <button
               onClick={() => {
                 navigate(`/main/games`);
-                setLoading(true);
               }}
               className={`${category === "games" && "active"} `}
             >
@@ -45,7 +28,6 @@ function App() {
             <button
               onClick={() => {
                 navigate(`/main/movies`);
-                setLoading(true);
               }}
               className={`${category === "movies" && "active"} `}
             >
